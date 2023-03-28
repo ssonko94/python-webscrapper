02487b3434cc618df315be2431278e8842f614cd
Jimmy@devs <ssonkojimmy@gmail.com>
2023-03-28 22:50:30 +0300

    (Run main python file program): run program for  the scrapper
    -Execute program from main file while importing the scrapper module

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from utils import helpers
 import scraper
+import trial_solution
 
 
 if __name__ == "__main__":
-    (is_success, message, text) = scraper.google_search("daily monitor")
-    links = scraper.scrape_for_links(text)
-    facebook_link = scraper.find_facebook_link(links, "daily monitor")
-    about_page = scraper.get_facebook_about_page(facebook_link)
-    address = scraper.find_email_address(about_page)
-    print(address)
+    # (is_success, message, text) = scraper.google_search("daily monitor")
+    # if is_success:
+    #     links = scraper.scrape_for_links(text)
+    #     facebook_link = scraper.find_facebook_link(links)
+    #     print(facebook_link)
+    #     (hasFound, string, html) = scraper.google_search(facebook_link)
+    #     anchors = scraper.scrape_for_links(html)
+    # print(anchors)
+    # (has_email, success_message, email) = scraper.find_email_address(html)
+    # print(email)
+    # links = scraper.get_links("https://www.facebook.com/DailyMonitor/")
+    # print(links)
+    trial_solution.main("./company_list.txt", "./company_email.txt")
