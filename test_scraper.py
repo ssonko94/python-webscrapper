77048df03c7906346bad7b9931bdccfb58ad9bdc
Jimmy@devs <ssonkojimmy@gmail.com>
2023-07-04 09:46:33 +0300

    (Update tests): Add unit tests for new functionality
    - Refactor code and remove old function unit tests then update the unit tests by adding tests for new functions

--- test_scraper.py
+++ test_scraper.py
@@ -79,74 +79,48 @@ def test_scrape_for_links(html, expected):
     assert scraper.scrape_for_links(html) == expected
 
 
-@pytest.mark.skip(reason="still failing")
 @pytest.mark.parametrize(
-    "links, expected_output",
-    [
-        ([], "No links to scrape"),
-        (["https://www.facebook.com", "https://www.twitter.com"], "facebook.com"),
-        (["https://www.google.com", "https://www.facebook.com"], "facebook.com"),
-        (["https://www.google.com", "https://www.youtube.com"], ""),
-    ],
-)
-def test_find_facebook_link(links, expected_output):
-    assert scraper.find_facebook_link(links) == expected_output
-
-
-@patch("scraper.requests")
-@pytest.mark.parametrize(
-    "url, expected_output",
-    [
-        ("https://www.facebook.com", 'b\'<!DOCTYPE html>\\n<html lang="en"...'),
-        ("https://www.facebook.com/nonexistent_page", "couldnot find this page"),
-        ("", "couldnot find this page"),
-        ("No links to scrape", "couldnot find this page"),
-    ],
-)
-def test_get_facebook_about_page(mock_requests, url, expected_output):
-    mock_response = _mock_response(content=expected_output)
-    mock_requests.get.return_value = mock_response
-    assert scraper.get_facebook_about_page(url) == expected_output
-
-
-@pytest.mark.parametrize(
-    "name, args, want",
+    "links, expected_username",
     [
         (
-            "success",
-            """<a  href="mailto:digital@ug.nationmedia.com" role="link" tabindex="0" target="_blank">digital@ug.nationmedia.com</a>""",
-            (
-                True,
-                "",
-                "digital@ug.nationmedia.com",
-            ),
+            [
+                "/url?q=https://www.facebook.com/username1&sa=U",
+                "/url?q=https://www.google.com&sa=U",
+            ],
+            "username1",
         ),
         (
-            "faliure",
-            "",
-            (False, "no email found", ""),
+            [
+                "/url?q=https://www.google.com&sa=U",
+                "/url?q=https://www.facebook.com/username2&sa=U",
+            ],
+            "username2",
         ),
+        ([], None),
     ],
 )
-def test_find_email_adress(name, args, want):
-    got = scraper.find_email_address(args)
-    assert got == want, "{}: got: {}, want: {}".format(name, got, want)
+def test_find_facebook_username(links, expected_username):
+    username = scraper.find_facebook_username(links)
+    assert username == expected_username
 
 
 @pytest.mark.parametrize(
-    "link, expected_output",
+    "facebook_username, expected_email",
     [
-        ("https://www.facebook.com/url?q=https://example.com&", "https://example.com"),
-        ("https://www.facebook.com/url?q=https://google.com&", "https://google.com"),
-        ("https://www.facebook.com/url?q=http://example.org&", "http://example.org"),
-        (
-            "https://www.facebook.com/url?q=http://example.com/path?query=value&",
-            "http://example.com/path?query=value",
-        ),
-        ("https://www.facebook.com/url?q=https://invalid-url", "no match found"),
-        ("https://www.facebook.com/url", "no match found"),
-        ("https://www.facebook.com", "no match found"),
+        ("existing_username", "example@example.com"),
+        ("non_existing_username", "no email found"),
+        (None, ""),
     ],
 )
-def test_trim_facebook_url(link, expected_output):
-    assert scraper.trim_facebook_url(link) == expected_output
+@patch("selenium.webdriver.Firefox")
+def test_scrape_email_from_about_page(mock_browser, facebook_username, expected_email):
+    mock_driver = mock_browser.return_value
+    mock_element = (
+        Mock(text="example@example.com")
+        if facebook_username == "existing_username"
+        else None
+    )
+    mock_driver.find_element.return_value = mock_element
+
+    email = scraper.scrape_email_from_about_page(facebook_username)
+    assert email == expected_email
